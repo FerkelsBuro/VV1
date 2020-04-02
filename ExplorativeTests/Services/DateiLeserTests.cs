42290649f39b150ece53d2540f310439cb5b6c39
ll9 <llwierer@web.de>
2020-04-02 14:57:29 +0200

    explorative tests

---
+++ ExplorativeTests/Services/DateiLeserTests.cs
@@ -0,0 +1,20 @@
+﻿using Infrastructure;
+using System;
+using System.IO;
+using Xunit;
+
+namespace ExplorativeTests.Services
+{
+    public class DateiLeserTests
+    {
+        [Fact]
+        public void GetFileTime_Test()
+        {
+            var leser = new DateiLeser();
+            var path = Path.Combine(Environment.CurrentDirectory, @"assets\ferkel.txt");
+            var time = leser.GetFileTime(path);
+
+            Assert.NotEqual(default(DateTime), time);
+        }
+    }
+}

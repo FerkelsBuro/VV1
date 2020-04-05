eff2ea312d63a2a57d7bacc89f45e2efec7ed095
Alexander Wierer <alexander.wierer@stud.fh-rosenheim.de>
2020-04-05 14:56:06 +0200

    watchedirectory

---
+++ Domain/Models/WatchedDirectory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Models
+{
+    class WatchedDirectory
+    {
+        public Dictionary<string, WatchedFile > watchedFiles { get; set; }
+
+        public void Update(FileEvent ev)
+        {
+
+        }
+    }
+}
